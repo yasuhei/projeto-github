08f85ddbc3277d1c0167c66026955d4b6b2d7a5c
yasuhei cristiano nakamura <yasuhei_nakamura@hotmail.com>
2021-09-01 18:10:55 -0300

    3 aula gostack

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,62 @@
 import React from 'react';
-import { Title } from './styles'
+import { FiChevronRight } from 'react-icons/fi'
+
+import logo from '../../assets/logo.svg'
+
+import { Title, Form, Repositories} from './styles'
 
 const Dashboard:  React.FC = () => {
-  return <Title>Explore repósitorios no github</Title>
+ 
+  return(
+    <>
+    <img src={logo} alt='github explorer'/>
+     <Title>Explore repósitorios no github</Title>
+
+    <Form>
+      <input placeholder='Digite o nome do repósitorio' />
+      <button type='submit'>Pesquisar</button>
+    </Form>
+
+    <Repositories>
+    <a href="teste">
+    <img src="https://avatars.githubusercontent.com/u/67479901?v=4" 
+    alt="Yasuhei Nakamura" 
+    />
+    <div>
+      <strong>rocketseat/unform</strong>
+      <p>Easy peasy highly scalable React</p>
+    </div>
+    <FiChevronRight size={20}/>
+    </a>
+
+    <a href="teste">
+    <img src="https://avatars.githubusercontent.com/u/67479901?v=4" 
+    alt="Yasuhei Nakamura" 
+    />
+    <div>
+      <strong>rocketseat/unform</strong>
+      <p>Easy peasy highly scalable React</p>
+    </div>
+    <FiChevronRight size={20}/>
+    </a>
+
+    <a href="teste">
+    <img src="https://avatars.githubusercontent.com/u/67479901?v=4" 
+    alt="Yasuhei Nakamura" 
+    />
+    <div>
+      <strong>rocketseat/unform</strong>
+      <p>Easy peasy highly scalable React</p>
+    </div>
+    <FiChevronRight size={20}/>
+    </a>
+    </Repositories>
+     </>
+  )
+
+
+ 
+  
 }
 
 export default Dashboard;
