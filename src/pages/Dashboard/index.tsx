e06a22eb352a859e661e94d20a99d0c16141fa83
yasuhei cristiano nakamura <yasuhei_nakamura@hotmail.com>
2021-08-31 22:57:09 -0300

    2 aula gostack

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './styles'
+
+const Dashboard:  React.FC = () => {
+  return <Title>Explore repósitorios no github</Title>
+}
+
+export default Dashboard;
