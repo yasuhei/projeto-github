fa1d6b5e186b41a69e4c5675abe7804987bad523
yasuhei cristiano nakamura <yasuhei_nakamura@hotmail.com>
2021-09-03 17:22:58 -0300

    aula 06

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect }  from 'react';
 import { FiChevronRight } from 'react-icons/fi'
 import api from '../../services/api'
 import logo from '../../assets/logo.svg'
@@ -18,7 +18,23 @@ interface Repository {
 const Dashboard:  React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
- const [repositories, setRepositories] = useState<Repository[]>([]);
+ const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+    if(storagedRepositories) {
+      return JSON.parse(storagedRepositories)
+    } else {
+      return[];
+    }
+
+ });
+
+ useEffect(() => {
+   localStorage.setItem(
+     '@GithubExplorer:repositories',
+     JSON.stringify(repositories),
+     );
+ },[repositories])
 
  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise <void> {
   event.preventDefault();
